a9a62c44d2cfc0ff7553a77d3451f4d02d254cd7
俊洋洋 <571172886@qq.com>
2020-06-16 10:17:04 +0800

    a

--- Example/Pods/Target Support Files/Pods-JYYTool_Example/Pods-JYYTool_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-JYYTool_Example/Pods-JYYTool_Example-frameworks.sh
@@ -162,11 +162,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/JYYTool/JYYTool.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/JYYTool/JYYTool.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- JYYTool.podspec
+++ JYYTool.podspec
@@ -38,5 +38,5 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-   s.dependency 'MBProgressHUD', '~> 1.2.0'
+  # s.dependency 'MBProgressHUD', '~> 1.2.0'
 end
